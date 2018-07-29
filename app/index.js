1c4a63c97ac3bb53eb0940b86f500b6162e01217
m.morar <m.morar@verta.media>
2018-07-29 22:32:36 +0300

    init project

---
+++ app/index.js
@@ -0,0 +1,141 @@
+import './assets/css/index.scss';
+import 'angular';
+
+const app = angular.module("appStarter", []);
+
+app.controller('controller', function($scope) {
+    $scope.tab = 1;
+    $scope.setTab = function(newTab) {
+        $scope.tab = newTab;
+    };
+    $scope.isSet = function(tabNum) {
+        return $scope.tab === tabNum;
+    };
+
+    $scope.price = {
+        model: null,
+        availableOptions: [
+            {id: 1, label: '1', sale: 0},
+            {id: 2, label: '2', sale: 0.1},
+            {id: 3, label: '3', sale: 0.2}
+        ],
+        selectedOption: {id: 1, label: '1', sale: 0},
+        totalSumm: 0,
+        price: 10,
+        promoSale: 0.5
+
+    };
+    $scope.regex = 'Test';
+    $scope.onLicenseCountChange = function() {
+        const price = $scope.price;
+        const sale = price.selectedOption.sale;
+        const promoSale = price.promoSale;
+
+        let promocode = $scope.promocode;
+        let code = 'Test';
+
+        price.totalSumm = price.price - (price.price * sale);
+        if (promocode === code) {
+            price.totalSumm = price.price - (price.price * price.promoSale)
+        } else {
+            $scope.errorMsg = 'asdasd'
+        }
+    };
+    $scope.onLicenseCountChange();
+    $scope.submitForm = function() {
+
+        // check to make sure the form is completely valid
+        if ($scope.userForm.$valid) {
+            alert('our form is amazing');
+        }
+
+    };
+
+    $scope.countries = [
+        {
+            "id": "1",
+            "name": "Australia"
+        }, {
+            "id": "2",
+            "name": "Spain"
+        }, {
+            "id": "3",
+            "name": "Ukraine"
+        }, {
+            "id": "4",
+            "name": "United States"
+        }
+    ];
+    $scope.city = [
+        {
+            "countryId": 1,
+            "name": "Abbotsford"
+        }, {
+            "countryId": 1,
+            "name": "Ballan"
+        }, {
+            "countryId": 1,
+            "name": "Colo Vale"
+        }, {
+            "countryId": 1,
+            "name": "Eagle Farm"
+        }, {
+            "countryId": 1,
+            "name": "Greenbank"
+        }, {
+            "countryId": 2,
+            "name": "Aguilar"
+        }, {
+            "countryId": 2,
+            "name": "Alicante"
+        }, {
+            "countryId": 2,
+            "name": "Aspe"
+        }, {
+            "countryId": 2,
+            "name": "Boiro"
+        }, {
+            "countryId": 2,
+            "name": "Caravaca"
+        }, {
+            "countryId": 3,
+            "name": "Kyiv"
+        }, {
+            "countryId": 3,
+            "name": "Odesa"
+        }, {
+            "countryId": 3,
+            "name": "Poltava"
+        }, {
+            "countryId": 3,
+            "name": "Sumy"
+        }, {
+            "countryId": 3,
+            "name": "Kryvyy Rih"
+        }, {
+            "countryId": 4,
+            "name": "Albert City"
+        }, {
+            "countryId": 4,
+            "name": "Belleville"
+        }, {
+            "countryId": 4,
+            "name": "Deckerville"
+        }, {
+            "countryId": 4,
+            "name": "Flintstone"
+        }, {
+            "countryId": 4,
+            "name": "Jackpot"
+        }
+    ];
+    $scope.updateCountry = function() {
+        $scope.availableCity = [];
+
+        angular.forEach($scope.city, function(value) {
+            if (value.countryId == $scope.avaliableCountry.id) {
+                $scope.availableCity.push(value);
+            }
+        });
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,146 @@
+var path = require('path');
+var webpack = require('webpack');
+var WebpackShellPlugin = require('webpack-shell-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+var postcssImport = require('postcss-smart-import');
+
+var env = process.env.NODE_ENV || 'development';
+var appPath = path.join(__dirname, './app');
+var wwwPath = path.join(__dirname, './www');
+
+var config = {
+    entry: path.join(appPath, 'index.js'),
+    output: {
+        path: path.join(wwwPath),
+        filename: 'bundle.[hash].js'
+    },
+    devServer: {
+        port: '9001',
+        contentBase: 'www',
+        historyApiFallback: true,
+        compress: true,
+        inline: true,
+        watchContentBase: true,
+        filename: 'bundle.js',
+        hot: true,
+        stats: {colors: true},
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000,
+            ignored: /node_modules/
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /.*\.html$/,
+                exclude: /index\.html/,
+                use: [
+                    'raw-loader'
+                ]
+            },
+            {
+                test: /\.(css|scss|sass)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'sass-loader',
+                        'resolve-url-loader'
+                    ]
+                })
+            },
+            {
+                test: /\.(jpe?g|png|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            hash: 'sha512',
+                            digest: 'hex',
+                            name: '[hash].[ext]'
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        query: {
+                            mozjpeg: {
+                                progressive: true
+                            },
+                            gifsicle: {
+                                interlaced: false
+                            },
+                            optipng: {
+                                optimizationLevel: 4
+                            },
+                            pngquant: {
+                                quality: '75-90',
+                                speed: 3
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolveLoader: {
+        moduleExtensions: ['-loader']
+    },
+    devtool: 'eval',
+    plugins: [
+       new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false,
+            options: {
+                postcss: [
+                    postcssImport({
+                        addDependencyTo: webpack
+                    }),
+                    precss,
+                    autoprefixer({
+                        browsers: ['last 5 versions', 'ie >= 9']
+                    })
+                ]
+            }
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: path.join(appPath, 'assets/img'),
+                to: path.join('assets', 'img')
+            }
+        ]),
+        new ExtractTextPlugin({
+            filename: 'index.[contenthash].css',
+            allChunks: true
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(appPath, 'index.html'),
+            inject: 'head',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                sequences: true,
+                booleans: true,
+                loops: true,
+                unused: true,
+                unsafe: true,
+                warnings: false
+            },
+            mangle: env !== 'development',
+            output: {
+                comments: false
+            }
+        })
+    ]
+};
+
+module.exports = config;
